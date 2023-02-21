a74df4c7cedcb0e9c8d6cb84b3d6dff2fa6e1dbb
hoon7566 <hoon7566@gmail.com>
2023-02-22 01:24:40 +0900

    U n i 완성

--- u_and_i/lib/main.dart
+++ u_and_i/lib/main.dart
@@ -3,7 +3,74 @@ import 'package:u_and_i/screen/home_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.pink[100],
+        accentColor: Colors.pink[100],
+        scaffoldBackgroundColor: Colors.pink[100],
+        textTheme: TextTheme(
+          displayLarge: TextStyle(
+            fontSize: 80,
+            fontFamily: 'parisienne',
+            color: Colors.white,
+          ),
+          displayMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          displaySmall: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          headlineMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          headlineSmall: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          titleLarge: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          bodyLarge: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          bodyMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          titleMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          titleSmall: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          bodySmall: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          labelSmall: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
       home: HomeScreen()
     )
   );
--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,29 +9,128 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  DateTime dateTime = DateTime.parse("2020-07-22");
+
+  Duration duration = DateTime.now().difference(DateTime.parse("2020-07-22"));
+
+  void onHeartPressed() {
+    showCupertinoDialog(
+        barrierDismissible: true,
+        context: context,
+        builder: (BuildContext context) {
+          return Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+                height: 300,
+                color: Colors.white,
+                child: CupertinoDatePicker(
+                  mode: CupertinoDatePickerMode.date,
+                  initialDateTime: DateTime.now(),
+                  maximumDate: DateTime.now(),
+                  onDateTimeChanged: (DateTime newDateTime) {
+                    setState(() {
+                      dateTime = newDateTime;
+                      duration = dateTime.difference(DateTime.now());
+                    });
+                  },
+                )),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.pink[100],
       body: SafeArea(
+        bottom: false,
         child: Container(
           width: MediaQuery.of(context).size.width,
           child: Column(
             children: [
-              Text('Hello World'
-                  ,style: TextStyle(
-                    fontSize: 80,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'parisienne',
-                    color: Colors.white,
-                  ),
-              ),
-              Text('Hello World'),
-              Text('Hello World'),
-              IconButton(onPressed: (){}, icon: Icon(Icons.favorite)),
+              _TopPart(
+                  dateTime: dateTime,
+                  duration: duration,
+                  onPressed: onHeartPressed),
+              _BottomPart(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _TopPart extends StatelessWidget {
+  final DateTime dateTime;
+  final Duration duration;
+  final VoidCallback onPressed;
+
+  _TopPart(
+      {required this.dateTime,
+      required this.duration,
+      required this.onPressed,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text('U&I', style: theme.textTheme.displayLarge),
+          Column(children: [
+            Text(
+              '우리 처음 만난날',
+              style: TextStyle(
+                fontSize: 30,
+                fontFamily: 'sunflower',
+                fontWeight: FontWeight.w200,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              '${dateTime.year}.${dateTime.month}.${dateTime.day}.',
+              style: TextStyle(
+                fontSize: 20,
+                fontFamily: 'sunflower',
+                fontWeight: FontWeight.w200,
+                color: Colors.white,
+              ),
+            )
+          ]),
+          IconButton(
+            icon: Icon(Icons.favorite),
+            iconSize: 50,
+            color: Colors.red,
+            onPressed: onPressed,
+          ),
+          Text(
+            'D+${duration.inDays}',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w500,
+              fontFamily: 'sunflower',
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BottomPart extends StatelessWidget {
+  const _BottomPart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: const Image(
+        image: AssetImage('asset/img/middle_image.png'),
       ),
     );
   }
