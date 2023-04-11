a86225fd783f4d76cbe15df329c6778e82878a2d
hoon7566 <hoon7566@gmail.com>
2023-04-11 19:01:49 +0900

    중간 commit

--- actual/lib/common/component/CustomTextFormField.dart
+++ actual/lib/common/component/CustomTextFormField.dart
@@ -5,28 +5,35 @@ class CustomTextFormField extends StatelessWidget {
   //hintText를 파라미터로 받음
   final String? hintText;
   final String? errorText;
+  final bool obscureText;
+  final bool autofocus;
+  final ValueChanged<String>? onChanged;
 
-  const CustomTextFormField({this.hintText,this.errorText, Key? key}) : super(key: key);
+  const CustomTextFormField({this.hintText,
+    this.errorText,
+    this.obscureText = false,
+    this.autofocus = false,
+    required this.onChanged,
+    Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final baseBorder = OutlineInputBorder(
-      borderSide: BorderSide(
-        color: INPUT_BORDER_COLOR,
-        width: 1.0
-      ),
+      borderSide: BorderSide(color: INPUT_BORDER_COLOR, width: 1.0),
     );
 
     return TextFormField(
+      obscureText: obscureText,
+      autofocus: autofocus,
+      onChanged: onChanged,
       decoration: InputDecoration(
+
         hintText: hintText,
         errorText: errorText,
-        hintStyle: TextStyle(
-          color: BODY_TEXT_COLOR,
-          fontSize: 14.0
-        ),
+        hintStyle: TextStyle(color: BODY_TEXT_COLOR, fontSize: 14.0),
         contentPadding:
-            const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
         fillColor: INPUT_BG_COLOR,
         filled: true,
         border: baseBorder,
