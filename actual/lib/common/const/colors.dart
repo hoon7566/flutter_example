bc87d08baf349a88b1ccdc8f809a1bb3a5ac1f69
hoon7566 <hoon7566@gmail.com>
2023-04-11 17:46:48 +0900

    중간 commit

--- actual/lib/common/const/colors.dart
+++ actual/lib/common/const/colors.dart
@@ -5,6 +5,6 @@ const PRIMARY_COLOR = Color(0xFF22A45D);
 // 글자 색상
 const BODY_TEXT_COLOR = Color(0xFF868686);
 // 텍스트 필드 색상
-const INPUR_BG_COLOR = Color(0xFFFBFBFB);
+const INPUT_BG_COLOR = Color(0xFFFBFBFB);
 // 텍스트 필드 테두리 색상
 const INPUT_BORDER_COLOR = Color(0xFFF3F2F2);
