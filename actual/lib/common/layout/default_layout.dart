7201eae171845d21650386a7ad7ff2cd9a70f328
hoon7566 <hoon7566@gmail.com>
2023-04-27 13:38:32 +0900

    Appbar, navigationBar

--- actual/lib/common/layout/default_layout.dart
+++ actual/lib/common/layout/default_layout.dart
@@ -1,17 +1,45 @@
+import 'package:actual/common/const/colors.dart';
 import 'package:flutter/material.dart';
 
 class DefaultLayout extends StatelessWidget {
-  final Color? backgroundColor;
   final Widget child;
+  final Color? backgroundColor;
+  final String? title;
+  final Widget? bottomNavigationBar;
 
-  const DefaultLayout({required this.child, this.backgroundColor, Key? key})
+  const DefaultLayout(
+      {required this.child,
+      this.backgroundColor,
+      this.title,
+      this.bottomNavigationBar,
+      Key? key})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: renderAppBar(),
       backgroundColor: backgroundColor ?? Colors.white,
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    }
+
+    return AppBar(
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.white,
+        elevation: 0.0, //  그림자 제거   튀어나오는 효과를 주는건데 디자인적으로 요새는 없는게 트랜드라서 없앤다.
+        title: Text(
+          title!,
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ));
   }
 }
--- actual/lib/common/view/root_tab.dart
+++ actual/lib/common/view/root_tab.dart
@@ -1,11 +1,47 @@
 import 'package:actual/common/layout/default_layout.dart';
 import 'package:flutter/material.dart';
 
-class RootTab extends StatelessWidget {
+import '../const/colors.dart';
+
+class RootTab extends StatefulWidget {
   const RootTab({Key? key}) : super(key: key);
 
   @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int _selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultLayout(child: Center(child: Text('RootTab')));
+    return DefaultLayout(
+      title: '코팩 딜리버리', child: Center(child: Text('RootTab'))
+      ,
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: PRIMARY_COLOR,
+        unselectedItemColor: BODY_TEXT_COLOR,
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        type: BottomNavigationBarType.fixed, // 기본이 shifting임 (shifting은 선택했을때 쫌더 커짐)
+        onTap: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        currentIndex: _selectedIndex,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), label: '홈'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search_outlined), label: '검색'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart_outlined), label: '장바구니'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline), label: '마이페이지'),
+        ],
+      ),
+
+    );
   }
 }
