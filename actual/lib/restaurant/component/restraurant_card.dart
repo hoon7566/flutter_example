465e4cc0b6d325220172ac4667192c08e85bd2ae
hoon7566 <hoon7566@gmail.com>
2023-05-02 01:08:03 +0900

    FutureBuilder로 server에서 가져온 데이터 ListView로 뿌려주기! image server에 있는 데이터로 가져오기

--- actual/lib/restaurant/component/restraurant_card.dart
+++ actual/lib/restaurant/component/restraurant_card.dart
@@ -12,7 +12,7 @@ class RestaurantCard extends StatelessWidget {
   final List<String> tags;
 
   // 평점 갯수
-  final int ratingCount;
+  final int ratingsCount;
 
   // 배달시간
   final int deliveryTime;
@@ -21,17 +21,17 @@ class RestaurantCard extends StatelessWidget {
   final int deliveryFee;
 
   // 평점
-  final double rating;
+  final double ratings;
 
   const RestaurantCard({
     Key? key,
     required this.image,
     required this.name,
     required this.tags,
-    required this.ratingCount,
+    required this.ratingsCount,
     required this.deliveryTime,
     required this.deliveryFee,
-    required this.rating,
+    required this.ratings,
   }) : super(key: key);
 
   @override
@@ -62,9 +62,9 @@ class RestaurantCard extends StatelessWidget {
         const SizedBox(height: 8.0),
         Row(
           children: [
-            _IconText(icon: Icons.star, label: rating.toString()),
+            _IconText(icon: Icons.star, label: ratings.toString()),
             renderDot(),
-            _IconText(icon: Icons.receipt, label: ratingCount.toString()),
+            _IconText(icon: Icons.receipt, label: ratingsCount.toString()),
             renderDot(),
             _IconText(icon: Icons.timelapse_outlined, label: '$deliveryTime 분'),
             renderDot(),
--- actual/lib/restaurant/view/restraurant_screen.dart
+++ actual/lib/restaurant/view/restraurant_screen.dart
@@ -1,4 +1,6 @@
+import 'package:actual/common/const/data.dart';
 import 'package:actual/restaurant/component/restraurant_card.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class RestaurantScreen extends StatelessWidget {
@@ -6,20 +8,61 @@ class RestaurantScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
+    Future<List> paginateRestaurant() async {
+      final dio = Dio();
+
+      final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+      final resp = await dio.get("http://$SERVER_IP/restaurant",
+          options: Options(headers: {
+            'authorization': 'Bearer $accessToken',
+          }));
+
+      return resp.data['data'];
+    }
+
+    return Container(
+      child: Center(
+        child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: RestaurantCard(
-              image: Image.asset("asset/img/food/ddeok_bok_gi.jpg" , fit: BoxFit.cover,), // 이래야 전체를 차지함
-              name: "불타는 떡볶이",
-              tags: ["떡볶이", "치즈", "매운 맛"],
-              ratingCount: 32,
-              deliveryTime: 30,
-              deliveryFee: 2500,
-              rating: 4.5),
+          child: FutureBuilder<List>(
+            future: paginateRestaurant(),
+            builder: (context, AsyncSnapshot<List> snapshot) {
+              print(snapshot.data);
+              print(snapshot.error);
+
+              if (!snapshot.hasData) {
+                return  CircularProgressIndicator();
+              }
+
+              return ListView.separated(
+                  itemBuilder: (context, index) {
+                    final item = snapshot.data![index];
+
+                    return RestaurantCard(
+                        image: Image.network(
+                          "http://$SERVER_IP${item['thumbUrl']}",
+                          fit: BoxFit.cover,
+                        ),
+                        // 이래야 전체를 차지함
+                        name: item['name'],
+                        tags: List<String>.from(item['tags']),
+                        ratingsCount: item['ratingsCount'],
+                        deliveryTime: item['deliveryTime'] ,
+                        deliveryFee: item['deliveryFee'] ,
+                        ratings: item['ratings'] );
+                  },
+                  separatorBuilder: (context, index) {
+                    return SizedBox(
+                      height: 16,
+                    );
+                  },
+                  itemCount: snapshot.data!.length);
+
+            },
+          ),
         ),
-      ],
+      ),
     );
   }
 }
